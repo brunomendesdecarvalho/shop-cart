a2d1fdac7bc0ae0b5cb83f2e46d0181b56490d3f
Bruno <brunomendesccb@gmail.com>
2021-07-07 08:49:10 -0300

    Criada listagem com nome do carrinho e 'teste' como subtítulo

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,29 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Coin>> fetchCoins(http.Client client) async {
+Future<List<Cart>> fetchCarts(http.Client client) async {
   final response = await client
       .get(Uri.parse('https://mockend.com/brunomendesdecarvalho/shop-cart/carts'));
 
-  // Use the compute function to run parseCoins in a separate isolate.
-  return compute(parseCoins, response.body);
+  // Use the compute function to run parseCarts in a separate isolate.
+  return compute(parseCarts, response.body);
 }
 
-// A function that converts a response body into a List<Coin>.
-List<Coin> parseCoins(String responseBody) {
+// A function that converts a response body into a List<Cart>.
+List<Cart> parseCarts(String responseBody) {
   final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
 
-  return parsed.map<Coin>((json) => Coin.fromJson(json)).toList();
+  return parsed.map<Cart>((json) => Cart.fromJson(json)).toList();
 }
 
-class Coin {
+class Cart {
   final int id;
   final String owner;
   // final String lastName;
   // final String address;
   // final String tradesCount24Hr;
 
-  Coin({
+  Cart({
     required this.id,
     required this.owner,
     // required this.lastName,
@@ -35,8 +35,8 @@ class Coin {
     // required this.tradesCount24Hr,
   });
 
-  factory Coin.fromJson(Map<String, dynamic> json) {
-    return Coin(
+  factory Cart.fromJson(Map<String, dynamic> json) {
+    return Cart(
       id: json['id'] as int,
       owner: json['owner'] as String,
       // lastName: json['lastname'] as String,
@@ -51,19 +51,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Coins';
+    final appTitle = 'Carts';
 
     return MaterialApp(
       title: appTitle,
-      home: MyHomePage(title: appTitle),
+      home: CartsPage(title: appTitle),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class CartsPage extends StatelessWidget {
   final String title;
 
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  CartsPage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -71,13 +71,13 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: FutureBuilder<List<Coin>>(
-        future: fetchCoins(http.Client()),
+      body: FutureBuilder<List<Cart>>(
+        future: fetchCarts(http.Client()),
         builder: (context, snapshot) {
           if (snapshot.hasError) print(snapshot.error);
 
           return snapshot.hasData
-              ? CoinsList(Coins: snapshot.data!)
+              ? CartsList(Carts: snapshot.data!)
               : Center(child: CircularProgressIndicator());
         },
       ),
@@ -85,18 +85,15 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class CoinsList extends StatelessWidget {
-  final List<Coin> Coins;
+class CartsList extends StatelessWidget {
+  final List<Cart> Carts;
 
-  CoinsList({Key? key, required this.Coins}) : super(key: key);
+  CartsList({Key? key, required this.Carts}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      itemCount: Coins.length,
+    return ListView.builder(
+      itemCount: Carts.length,
       itemBuilder: (context, index) {
         return Column(
           children: <Widget>[
@@ -114,13 +111,19 @@ class CoinsList extends StatelessWidget {
                               title: Row(
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text('Coins',
+                                  Text('${Carts[index].owner}',
                                       style: TextStyle(
                                         fontSize: 16,
                                         color: Colors.cyan,
                                       )
                                   ),
                                 ],
+                              ),
+                              subtitle: Text('Teste',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.lightBlueAccent,
+                                  )
                               ),
                             )
                           ],
